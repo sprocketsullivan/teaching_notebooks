bed6a24a73cb34c92daee0e1fbe7e361453aac22
sprocketsullivan <toelch@gmail.com>
2017-05-16 15:26:32 +0200

    added python file

---
+++ read_data_dataverse.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 16 15:07:06 2017
+
+@author: toelch
+"""
+
+import pandas as pd
+
+data = pd.read_csv('https://dataverse.harvard.edu/api/access/datafile/3005330')
